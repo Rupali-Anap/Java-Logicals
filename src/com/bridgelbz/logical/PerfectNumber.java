2ae862b405189af6a7adcf2182f0f53114668cbf
Rupali Anap <anaprupali333@gmail.com>
2022-12-29 09:16:26 -0800

    Added: All logical Programs

---
+++ src/com/bridgelbz/logical/PerfectNumber.java
@@ -0,0 +1,27 @@
+package com.bridgelbz.logical;
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+            public static void main(String[] args) {
+
+            int n, sum = 0;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter any Number: ");
+            n = sc.nextInt();
+
+            for (int i=1; i<n; i++) {
+
+                if (n%i == 0) {
+                    sum = sum + i;
+                }
+            }
+
+            if (n == sum) {
+                System.out.println("Perfect Number");
+            } else {
+                System.out.println("Not a Perfect Number");
+            }
+        }
+    }
+
